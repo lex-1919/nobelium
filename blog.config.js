4b0b65f0b0c90abcd64f5bdf1b2b959ca5d63856
jialiang <37242793+lex-1919@users.noreply.github.com>
2022-05-26 15:02:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '梦之飞鸟',
+  author: 'jialiang',
+  email: 'tu798739016@gmail.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
